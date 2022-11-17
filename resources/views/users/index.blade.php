b5be6c7bbba29d692f663940f186393b6b0d123f
Hlanga <82909736+Hlangalwandile@users.noreply.github.com>
2022-11-17 08:04:53 +0200

    last last

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -21,14 +21,14 @@
             
               <div class="col-md-3">
                 <div class="card col bg-dark text-light p-0" style="width: 18rem;">
-                    <img class="card-img-top" src="https://dummyimage.com/600x400/000/fff" alt="Card image cap">
+                    
                     <div class="card-body">
                       <h5 class="card-title">{{$person->name}}</h5>
                       <h6 class="card-subtitle mb-2 text-muted">{{$person->name}}</h6>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <p class="card-text">{{$person->quote}}</p>
                     </div>
                     <div class="card-body">
-                      <a href="#" class="card-link btn btn-danger">Delete</a>
+                      <a href="{{route('users.editPage',[$person->id])}}" class="card-link btn btn-danger">Delete</a>
                       <a href="{{route('users.editPage',[$person->id])}}" class="card-link btn btn-secondary">Edit</a>
                     </div>
                   </div>
